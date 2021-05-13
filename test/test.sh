321441efe22c485161647025b6efd9611a1b6583
Magnus Haraldson Høie <39849954+Magnushhoie@users.noreply.github.com>
2021-05-13 23:11:53 +0200

    Fixed unit test bug

--- test/test.sh
+++ test/test.sh
@@ -2,22 +2,28 @@
 # bash_notes test
 # https://github.com/Magnushhoie/bash_notes
 
+shopt -s expand_aliases
+
+# Set directory variables
+script_path="${BASH_SOURCE[0]}"
 script_dir="$(realpath $(dirname "$script_path"))"
 tmp_dir=$script_dir/tmp
 main_dir=$(dirname $script_dir)
 
-shopt -s expand_aliases
-
-#
-# Initialization
-#
+# Set log files
+test_results=$script_dir/test_results
+test_logfile=$tmp_dir/test.log
 
 # Cleanup and reset tmp directory
 if ! [ -z "${tmp_dir}" ] && [ -d "$tmp_dir" ]
     then rm -r $tmp_dir/*
 fi
-rm -f $script_dir/test_results
-touch $script_dir/test_results
+
+# Delete+Create new log files
+rm -f $test_results
+rm -f $test_logfile
+touch $test_results
+touch $test_logfile
 
 # Copy project files to tmp directory
 mkdir -p $tmp_dir
@@ -27,27 +33,27 @@ cp $main_dir/references.txt $tmp_dir/
 cp $script_dir/test_config.txt $tmp_dir/config.txt
 
 #
-# setup.sh
+# test setup.sh, install script
 #
 
 # Run install script
-source $tmp_dir/setup.sh > tmp/test.log 2>&1
+source $tmp_dir/setup.sh > $test_logfile 2>&1
 cp $main_dir/references.txt $notes_folder/second_file.txt
 
 # Check install folder
-V=$(grep 'tmp/_bash_notes' $tmp_dir/test.log | wc -l | awk '{print $1}')
+V=$(grep 'tmp/_bash_notes' $test_logfile | wc -l | awk '{print $1}')
 if [[ $V == '2' ]]; then 
-    echo "1. PASS: Correct install folder." >> test_results
+    echo "1. PASS: Correct test install folder" >> $test_results
 else 
-    echo "1. FAIL: Wrong install folder." >> test_results
+    echo "1. FAIL: Wrong test install folder" >> $test_results
 fi 
 
 # Check install complete
-V=$(grep 'Done!' tmp/test.log)
+V=$(grep 'Done!' $test_logfile)
 if [[ $V == 'Done!' ]]; then 
-    echo "2. PASS: Install complete." >> test_results
+    echo "2. PASS: Install complete" >> $test_results
 else 
-    echo "2. FAIL: Install failed." >> test_results
+    echo "2. FAIL: Install failed" >> $test_results
 fi 
 
 # setup aliases manually
@@ -56,31 +62,31 @@ alias refe=$tmp_dir/src/refe.sh
 
 
 #
-# ref
+# test ref, search script
 #
 
 # ref search with keywords
 V=$(ref This is your references.txt file | wc | awk '{print $1}')
 if [ "$V" -gt "10" ]; then
-    echo "3. PASS: ref search keywords" >> test_results
+    echo "3. PASS: ref search keywords" >> $test_results
 else
-    echo "3. FAIL: ref search keywords" >> test_results
+    echo "3. FAIL: ref search keywords" >> $test_results
 fi
 
 # ref --list
 V=$(ref --list | grep "references" | wc -l | awk '{print $1}')
 if [ "$V" -eq "1" ]; then
-    echo "4. PASS: ref --list" >> test_results
+    echo "4. PASS: ref --list" >> $test_results
 else
-    echo "4. FAIL: ref --list" >> test_results
+    echo "4. FAIL: ref --list" >> $test_results
 fi
 
 # ref --help
 V=$(ref --help | grep "Example usage:" | wc -l | awk '{print $1}')
 if [ "$V" -eq "1" ]; then
-    echo "5. PASS: ref --help" >> test_results
+    echo "5. PASS: ref --help" >> $test_results
 else
-    echo "5. FAIL: ref --help" >> test_results
+    echo "5. FAIL: ref --help" >> $test_results
 fi
 
 # ref --open
@@ -90,22 +96,22 @@ open() {
 export -f open
 V=$(ref --open | grep "references.txt" | wc -l | awk '{print $1}')
 if [ "$V" -gt "0" ]; then
-    echo "6. PASS: ref --open" >> test_results
+    echo "6. PASS: ref --open" >> $test_results
 else
-    echo "6. FAIL: ref --open" >> test_results
+    echo "6. FAIL: ref --open" >> $test_results
 fi
 
 # ref --config
 V=$(ref --config | grep "config.txt" | wc -l | awk '{print $1}')
 if [ "$V" -gt "0" ]; then
-    echo "7. PASS: ref --config" >> test_results
+    echo "7. PASS: ref --config" >> $test_results
 else
-    echo "7. FAIL: ref --config" >> test_results
+    echo "7. FAIL: ref --config" >> $test_results
 fi
 
 
 #
-# refe
+# test refe, editor search/edit
 #
 
 # refe without keywords
@@ -116,38 +122,41 @@ export -f vim
 
 V=$(refe | wc | awk '{print $1}')
 if [ "$V" -eq "2" ]; then
-    echo "8. PASS: refe no keywords" >> test_results
+    echo "8. PASS: refe no keywords" >> $test_results
 else
-    echo "8. FAIL: refe no keywords" >> test_results
+    echo "8. FAIL: refe no keywords" >> $test_results
 fi
 
 # refe with keywords
 V=$(refe references.txt | grep "set hlsearch" | wc | awk '{print $1}')
 if [ "$V" -gt "0" ]; then
-    echo "9. PASS: refe search keywords" >> test_results
+    echo "9. PASS: refe search keywords" >> $test_results
 else
-    echo "9. FAIL: refe search keywords" >> test_results
+    echo "9. FAIL: refe search keywords" >> $test_results
 fi
 
 # refe create new file require .txt
 V=$(refe --new new_file | grep "must end with .txt" | wc | awk '{print $1}')
 if [ "$V" -eq "1" ]; then
-    echo "10. PASS: refe new file expected error message" >> test_results
+    echo "10. PASS: refe new file expected error message" >> $test_results
 else
-    echo "10. FAIL: refe new file expected error message" >> test_results
+    echo "10. FAIL: refe new file expected error message" >> $test_results
 fi
 
 # refe create new file
 V=$(refe --new new_file.txt | grep "new_file.txt" | wc -l | awk '{print $1}')
 if [ "$V" -eq "2" ]; then
-    echo "11. PASS: refe new file creation" >> test_results
+    echo "11. PASS: refe new file creation" >> $test_results
 else
-    echo "11. FAIL: refe new file creation" >> test_results
+    echo "11. FAIL: refe new file creation" >> $test_results
 fi
 
 V=$(refe second_file | grep "second_file.txt" | wc -l | awk '{print $1}')
 if [ "$V" -eq "2" ]; then
-    echo "12. PASS: refe alternative file" >> test_results
+    echo "12. PASS: refe alternative file" >> $test_results
 else
-    echo "12. FAIL: refe alternative file" >> test_results
+    echo "12. FAIL: refe alternative file" >> $test_results
 fi
+
+# Display test results
+cat $test_results
